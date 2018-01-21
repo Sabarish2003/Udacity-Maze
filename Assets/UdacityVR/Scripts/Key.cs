c4feab5ee92a073036e03244ed8e7fbe0d8be62e
Sabarish2003 <gnasa@utschools.ca>
2018-01-20 19:04:39 -0500

    Completed Project

---
+++ Assets/UdacityVR/Scripts/Key.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour 
+{
+    //Create a reference to the KeyPoofPrefab and Door
+    public GameObject keyPoof;
+    public GameObject door;
+    Door doorScript;
+
+    private bool keyCollect = false;
+
+	void Update()
+	{
+		//Not required, but for fun why not try adding a Key Floating Animation here :)
+	}
+
+	public void OnKeyClicked()
+	{
+        // Instatiate the KeyPoof Prefab where this key is located        
+        // Make sure the poof animates vertically
+        Instantiate(keyPoof, this.transform.position, this.transform.rotation);
+        // Call the Unlock() method on the Door
+        doorScript = door.GetComponent<Door>();
+        doorScript.Invoke("Unlock", 0);       
+        
+        // Set the Key Collected Variable to true
+        this.keyCollect = true;
+        // Destroy the key. Check the Unity documentation on how to use Destroy
+        Destroy(this.gameObject);
+    }
+
+}
